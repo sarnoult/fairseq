12c1e94c8ea6950dc5ef768fce02d1ec08940e04
sarnoult <sarnoult@users.noreply.github.com>
2023-11-28 09:33:25 +0100

    add sentencepiece in setup

--- setup.py
+++ setup.py
@@ -196,6 +196,7 @@ def do_setup(package_data):
             'numpy; python_version>="3.7"',
             "regex",
             "sacrebleu>=1.4.12",
+            "sentencepiece",
             "torch",
             "tqdm",
         ],
